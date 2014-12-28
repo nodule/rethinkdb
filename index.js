b0445a25e26dfe62ca83c1559ec1f3b02b4365dc
Rob Halff <rob.halff@gmail.com>
2014-12-28 22:29:03 +0100

    update

---
+++ index.js
@@ -0,0 +1,20 @@
+module.exports = {
+  api: require('./api'),
+  close: require('./close'),
+  connect: require('./connect'),
+  db: require('./db'),
+  delete: require('./delete'),
+  get: require('./get'),
+  insert: require('./insert'),
+  insertAll: require('./insertAll'),
+  reconnect: require('./reconnect'),
+  replace: require('./replace'),
+  run: require('./run'),
+  sync: require('./sync'),
+  table: require('./table'),
+  tableCreate: require('./tableCreate'),
+  tableDrop: require('./tableDrop'),
+  tableList: require('./tableList'),
+  update: require('./update'),
+  use: require('./use')
+}
